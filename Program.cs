e86bb604008a9e161354f97a3d20d57bbbd794b0
Khalmatov <large000@yandex.ru>
2021-09-24 23:29:19 +0300

    Добавил краткое описание о себе

--- Program.cs
+++ Program.cs
@@ -7,9 +7,17 @@ namespace Module_3_practical_task_1
         static void Main(string[] args)
         {
             string myName;
-            myName = "Ruslan";
+            myName = "Halmach";
+            ushort age = 34;
+            bool havePet = true;
+            float legSize = 40;
 
-            Console.WriteLine(myName);
+
+
+            Console.WriteLine("My name is {0}",myName);
+            Console.WriteLine("My age is {0}", age);
+            Console.WriteLine("Do I have a pet? {0}", havePet);
+            Console.WriteLine("My shoe size is {0}", legSize);
 
             Console.ReadKey();
         }
