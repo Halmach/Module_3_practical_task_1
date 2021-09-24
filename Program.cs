c7efa85d23c8e9f8fe9a195d761f6f6032efba84
Khalmatov <large000@yandex.ru>
2021-09-24 22:00:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Module_3_practical_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Ruslan";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
